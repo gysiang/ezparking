a14aa6cd3f4638b72f36b20a5690875572d0eca7
Goh Yong Siang <gysiang@gmail.com>
2022-07-10 00:10:02 +0800

    successfully loaded google maps and 1 marker.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
+
+function MapContainer () {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey:PUBLIC_GOOGLE_MAPS_API_KEY,
+   });
+
+   if (!isLoaded) return <div>Loading...</div>
+   return <Map/>
+}
+function Map() {
+  return (
+    <GoogleMap 
+    zoom={20}
+    center={{lat:1.3919935522366003,lng:103.89492287401924}}
+    mapContainerClassName="map-container">
+    <Marker position={{lat:1.3919935522366003,lng:103.89492287401924}}/>
+    </GoogleMap>
+  )
+}
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -15,6 +35,7 @@ export default function App() {
           <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
         )}
       </div>
+      <MapContainer />
     </div>
   );
 }
