9394b3fef204fe55e03b4e95442f64f831b44b82
Goh Yong Siang <gysiang@gmail.com>
2022-07-10 18:26:46 +0800

    converted geolocations to lat lng

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,81 @@
 import axios from "axios";
-import React, { useState, useEffect } from "react";
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+import React, { useState, useEffect, useMemo} from "react";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
 
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
+import svgCoverter from './svy21.js'
+
+// console.log(process.env.DB_DEV_NAME)
+
+import allCarparks from '../carparks-all.json'
+
+const { Result } = allCarparks;
+
+function getCarparks(locations) {
+  let ppCode = new Set ();
+  let uniques=[];
+   for (let location of locations) {
+        if (!ppCode.has(location.ppCode)) {
+            ppCode.add(location.ppCode);
+            uniques.push(location);
+        }
+    }
+    return uniques;
+}
+
+const filteredList = getCarparks(Result);
+
+
+const MarkersList = filteredList.map((locationPin,index) => {
+
+let cv = new svgCoverter();
+let north = (locationPin.geometries[0].coordinates.split(',')[0]);
+let east = (locationPin.geometries[0].coordinates.split(',')[1]);
+var resultLatLon = cv.computeLatLon(north, east);
+
+let position = {
+  lat:resultLatLon.lat,
+  lng:resultLatLon.lon,
+}
+console.log(position)
+
+  if (index <50){
+    return (
+      <li key={locationPin.ppCode}>
+        <Marker position={position}/>
+      </li>
+    )
+  }
+  return <></>;
+});
+
+
 function MapContainer () {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey:PUBLIC_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey:"AIzaSyAbXhFVcuk-ht7aTKSvtxK1_8zIp9gFLPw",
    });
 
    if (!isLoaded) return <div>Loading...</div>
    return <Map/>
 }
+
 function Map() {
+  const center = useMemo(() => ({
+    lat:1.3919935522366003,
+    lng:103.89492287401924}), []);
+
   return (
     <GoogleMap 
     zoom={20}
-    center={{lat:1.3919935522366003,lng:103.89492287401924}}
+    center={center}
     mapContainerClassName="map-container">
-    <Marker position={{lat:1.3919935522366003,lng:103.89492287401924}}/>
+    <ol>
+    <Marker position={{lat:1.3941679247385295,lng:103.88987098640447}}/>   
+    <Marker position={{lat:1.3908673566483016,lng:103.89684333566427}}/>  
+
+    {MarkersList}
+    </ol>
     </GoogleMap>
   )
 }
@@ -35,6 +92,7 @@ export default function App() {
           <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
         )}
       </div>
+      <br/>
       <MapContainer />
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './styles.scss';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
 import App from './App.jsx';
 
-// create an element that React will render stuff into
-const rootElement = document.createElement('div');
+// // create an element that React will render stuff into
+// const rootElement = document.createElement('div');
 
-// put that element onto the page
-document.body.appendChild(rootElement);
+// // put that element onto the page
+// document.body.appendChild(rootElement);
 
-// have react render the JSX element into the root element.
-render(<App />, rootElement);
+// // have react render the JSX element into the root element.
+// render(<App />, rootElement);
+
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement);
+
+root.render(
+    <App />
+);
