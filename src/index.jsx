b8fa93038ef6f51b5d4f69975de93b7def117048
Goh Yong Siang Ivan <37769720+gysiang@users.noreply.github.com>
2022-07-12 11:09:22 +0800

    commit before pulling.

--- src/index.jsx
+++ src/index.jsx
@@ -6,18 +6,12 @@ import {createRoot} from 'react-dom/client';
 
 import App from './App.jsx';
 
-// // create an element that React will render stuff into
-// const rootElement = document.createElement('div');
+// create an element that React will render stuff into
+const rootElement = document.createElement("div");
 
-// // put that element onto the page
-// document.body.appendChild(rootElement);
-
-// // have react render the JSX element into the root element.
-// render(<App />, rootElement);
-
-const rootElement = document.getElementById("root")
+// put that element onto the page
+document.body.appendChild(rootElement);
 const root = createRoot(rootElement);
 
-root.render(
-    <App />
-);
+// have react render the JSX element into the root element.
+root.render(<App />);
