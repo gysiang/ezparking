63d16bbf162dd2b8b731e5e7379cc0b1689de6fb
eva.wang <eva.wang81@gmail.com>
2022-07-15 11:32:29 +0800

    added logout style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import axios from "axios";
 
 export default function Navbar() {
   return (
-    <nav className="nav d-flex justify-content-between align-items-center">
+    <nav className="nav d-flex justify-content-between align-items-center px-3">
       <a href="">Avatar</a>
-      <a href="">Log out</a>
+      <a href="" className="logoutIcon">
+        <i class="bi bi-box-arrow-right"></i>
+      </a>
     </nav>
   );
 }
