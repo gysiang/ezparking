e8147f4f0a3accf85acc9010be879e33f731a511
eva.wang <eva.wang81@gmail.com>
2022-07-15 11:22:22 +0800

    added styles to signup page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -47,33 +47,47 @@ export default function Signup({ setDisplaySignupPage }) {
 
   return (
     <div className="loginDiv d-flex flex-column justify-content-center align-items-center">
-      <div className="d-flex flex-column">
+      <div className="d-flex flex-column border p-2 align-items-center rounded">
         <h5>Create account</h5>
+        <div className="my-1 userIcon">
+          <i className="bi bi-person-circle"></i>
+        </div>
+        <input
+          type="text"
+          value={userName}
+          onChange={userNameChange}
+          placeholder="Name"
+          className="form-control my-1"
+        />
+        <input
+          type="text"
+          value={userEmail}
+          onChange={userEmailChange}
+          placeholder="Email"
+          className="form-control my-1"
+        />
+        <input
+          type="password"
+          value={userPassword}
+          onChange={userPasswordChange}
+          placeholder="Password"
+          className="form-control my-1"
+        />
+        <button
+          type="button"
+          onClick={handleSignup}
+          className="form-control bg-primary my-1"
+        >
+          SIGN UP
+        </button>
+        <button
+          type="button"
+          onClick={handleSignin}
+          className="form-control bg-info my-1"
+        >
+          SIGN IN
+        </button>
       </div>
-      <input
-        type="text"
-        value={userName}
-        onChange={userNameChange}
-        placeholder="Name"
-      />
-      <input
-        type="text"
-        value={userEmail}
-        onChange={userEmailChange}
-        placeholder="Email"
-      />
-      <input
-        type="password"
-        value={userPassword}
-        onChange={userPasswordChange}
-        placeholder="Password"
-      />
-      <button type="button" onClick={handleSignup}>
-        SIGN UP
-      </button>
-      <button type="button" onClick={handleSignin}>
-        SIGN IN
-      </button>
     </div>
   );
 }
